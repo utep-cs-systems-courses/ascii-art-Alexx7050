207455ea3398af70dbd1f50e132679d05db18425
Alex Jaramillo <ajaramillo5@miners.utep.edu>
2022-06-14 22:55:12 -0600

    ascii draw

--- uimain.c
+++ uimain.c
@@ -28,6 +28,10 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      for (char c = 'A'; c < 'K'; c++)
+      {
+	print_char_5x7(c);
+      }
       break;
     case 'a':
       puts("You selected arrow");
